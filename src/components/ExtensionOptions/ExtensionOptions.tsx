55e4aa240617a44857fc488682403d0ff50ea8a4
Sergey Yakunin <sergey@yakunins.com>
2025-04-02 22:24:12 -0700

    version

--- src/components/ExtensionOptions/ExtensionOptions.tsx
+++ src/components/ExtensionOptions/ExtensionOptions.tsx
@@ -5,10 +5,23 @@ import "./extension-options.css";
 
 const author = {
     target: "_blank",
-    href: "http://github.com/yakunins",
+    href: "https://github.com/yakunins/digital-clock-new-tab-extension",
     children: "yakunins@github",
     mail: "mailto:s@yakunins.com",
 };
+
+declare const browser: typeof chrome;
+let version = "unknown";
+try {
+    if (chrome?.runtime) {
+        version = chrome?.runtime?.getManifest().version;
+    }
+    if (browser?.runtime) {
+        version = browser?.runtime?.getManifest().version;
+    }
+} catch (error) {
+    console.log(error);
+}
 
 type ExtensionOptionsProps = {
     origin?: SettingsForm["origin"];
@@ -19,7 +32,7 @@ export const ExtensionOptions = ({ origin }: ExtensionOptionsProps) => {
         <div className="extension-options">
             <SettingsForm origin={origin} />
             <p className="copy-info">
-                2022, 2024 © <a {...author} />
+                v{version} · 2025 · <a {...author} />
                 <br />
             </p>
         </div>
