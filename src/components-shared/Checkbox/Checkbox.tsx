db8723a2af8d26387db2b3ce049f2bda53f80fcd
Sergey Yakunin <sergey@yakunins.com>
2025-07-28 13:02:35 -0700

    checkbox component added

---
+++ src/components-shared/Checkbox/Checkbox.tsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect, useRef, useId, memo } from "react";
+import { Icon, Innout } from "../../components-shared";
+import { clsx } from "clsx";
+
+import "./checkbox.css";
+
+export interface Checkbox
+    extends Omit<
+        React.InputHTMLAttributes<HTMLInputElement>,
+        "checked" | "defaultValue" | "onChange"
+    > {
+    children?: React.ReactNode;
+    defaultValue?: boolean;
+    name?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Checkbox = memo(
+    ({ children, defaultValue, name, onChange, ...rest }: Checkbox) => {
+        const [isChecked, setChecked] = useState(defaultValue);
+        const [isFocused, setFocused] = useState(false);
+
+        const inputRef = useRef<HTMLInputElement>(null);
+        const id = rest.id || useId();
+
+        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+            setChecked(e.target.checked);
+            onChange?.(e);
+        };
+
+        const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+            if (e.key === "Enter" || e.key === "Space") {
+                inputRef?.current?.click();
+            }
+        };
+
+        useEffect(() => {
+            isFocused && inputRef?.current?.focus();
+        }, [isFocused]);
+
+        useEffect(() => {
+            setChecked(defaultValue);
+        }, [defaultValue]);
+
+        return (
+            <div
+                className={clsx(
+                    "checkbox",
+                    { checked: isChecked },
+                    { focused: isFocused },
+                    rest.className
+                )}
+            >
+                <input
+                    {...rest}
+                    className="visually-hidden"
+                    ref={inputRef}
+                    type="checkbox"
+                    id={id}
+                    checked={isChecked}
+                    name={name}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    onFocus={() => setFocused(true)}
+                    onBlur={() => setFocused(false)}
+                />
+                <span className="checked-indicator"></span>
+                <label htmlFor={id}>{children}</label>
+            </div>
+        );
+    }
+);
