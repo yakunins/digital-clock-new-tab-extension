5b62a085b29ef3c0acceddc170a441c7df29ae75
Sergey Yakunin <sergey@yakunins.com>
2025-08-14 23:59:10 -0700

    useHasScrollbar hook

---
+++ src/hooks/useHasScrollbar/useHasScrollbar.ts
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+
+type HasScrollbar = "true" | "false" | "unknown";
+type MeasurementEventType = string | undefined;
+
+type UseHasScrollbar = [HasScrollbar] | [HasScrollbar, MeasurementEventType];
+
+const initial: HasScrollbar = "unknown";
+
+export const useHasScrollbar = (
+    ref: React.RefObject<HTMLElement>,
+    axis: "y" | "x" = "y",
+    dependecies: any[] = []
+): UseHasScrollbar => {
+    const [hasScrollbar, setHasScrollbar] = useState<HasScrollbar>(initial);
+    const [eventType, setEventType] = useState<MeasurementEventType>();
+
+    let initialResizeHandled = false;
+
+    const checkScrollbar = (eventType?: string) => {
+        if (!ref.current) {
+            setHasScrollbar(initial);
+            return;
+        }
+        const result =
+            axis === "y"
+                ? ref.current.scrollHeight > ref.current.clientHeight
+                : ref.current.scrollWidth > ref.current.clientWidth;
+
+        const resultStr = result ? "true" : "false";
+
+        if (!initialResizeHandled && eventType === "resize") {
+            initialResizeHandled = true;
+            setHasScrollbar(resultStr);
+        } else {
+            setEventType(eventType);
+            setHasScrollbar(resultStr);
+        }
+    };
+
+    useEffect(() => {
+        if (!ref.current) {
+            setHasScrollbar(initial);
+            return;
+        }
+        checkScrollbar("initial");
+
+        const resizeObserver = new ResizeObserver(() =>
+            checkScrollbar("resize")
+        );
+        resizeObserver.observe(ref.current);
+
+        const mutationObserver = new MutationObserver(() =>
+            checkScrollbar("mutation")
+        );
+        mutationObserver.observe(ref.current, {
+            childList: true,
+            subtree: true,
+            characterData: true,
+        });
+
+        return () => {
+            resizeObserver.disconnect();
+            mutationObserver.disconnect();
+        };
+    }, [ref, ...dependecies]);
+
+    return [hasScrollbar, eventType];
+};
